f8d7a676d8256f041f4263e829c73bb4e6936f1c
SaraMøllerØstergaard#1413 <sara.m.oestergaard@gmail.com>
2023-03-22 13:57:04 +0100

    fit rl to dyads

--- src/fit_model.r
+++ src/fit_model.r
@@ -30,10 +30,16 @@ df <- read_csv("../data/AlienData.csv") %>%
         f4 = as.numeric(substr(stimulus, 4, 4)),
         f5 = as.numeric(substr(stimulus, 5, 5)),
         danger_response = ifelse(response == 3 | response == 4, 1, 0),
-        nutri_response = ifelse(response == 2 | response == 4, 1, 0)
+        nutricious_response = ifelse(response == 2 | response == 4, 1, 0)
     ) %>%
-    filter( # include only conditions with individuals and low complexity session
-        condition == 1 & session == 1
+    filter( # include only low complexity session
+        session == 1
+    ) %>%
+    mutate( # make unique subject ids
+        subject = ifelse(condition == 2,
+            subject + 100,
+            subject
+        )
     )
 
 
@@ -54,53 +60,62 @@ print("Fitting model")
 for (sub in unique(df$subject)) {
     print(paste("Subject =", sub))
 
-    # filter data
-    tmp <- df %>%
-        filter(subject == sub)
-    observations <- tmp %>%
-        select(
-            c("f1", "f2", "f3", "f4", "f5")
+    for (ses in unique(df$session)) {
+        print(paste("Session =", ses))
+
+        # filter data
+        tmp <- df %>%
+            filter(
+                subject == sub,
+                session == ses
+            )
+        observations <- tmp %>%
+            select(
+                c("f1", "f2", "f3", "f4", "f5")
+            )
+
+        # input data for model
+        if (model == "gcm") {
+            print("script can run with GCM yet")
+        } else if (model == "rl") {
+            data <- list(
+                ntrials = nrow(observations),
+                nfeatures = ncol(observations),
+                cat_one = tmp$nutricious,
+                y = tmp$nutricious_response,
+                obs = as.matrix(observations),
+                alpha_neg_prior_values = c(0, 1),
+                alpha_pos_prior_values = c(0, 1),
+                temp_prior_values = c(0, 1)
+            )
+        }
+
+
+        # sample model
+        samples <- mod$sample(
+            data = data,
+            seed = 123,
+            chains = 2,
+            parallel_chains = 2,
+            threads_per_chain = 2,
+            iter_warmup = 1000,
+            iter_sampling = 2000,
+            refresh = 1000,
+            max_treedepth = 20,
+            adapt_delta = 0.99
         )
 
-    # input data for model
-    if (model == "gcm") {
-        print("script can run with GCM yet")
-    } else if (model == "rl") {
-        data <- list(
-            ntrials = nrow(observations),
-            nfeatures = ncol(observations),
-            cat_one = tmp$nutricious,
-            y = tmp$nutri_response,
-            obs = as.matrix(observations),
-            alpha_neg_prior_values = c(0, 1),
-            alpha_pos_prior_values = c(0, 1),
-            temp_prior_values = c(0, 1)
-        )
-    }
+        # save results
+        draws_df <- as_draws_df(samples$draws())
+        draws_df$condition <- con
+        draws_df$subject <- sub
+        draws_df$session <- ses
 
-
-    # sample model
-    samples <- mod$sample(
-        data = data,
-        seed = 123,
-        chains = 2,
-        parallel_chains = 2,
-        threads_per_chain = 2,
-        iter_warmup = 1000,
-        iter_sampling = 2000,
-        refresh = 1000,
-        max_treedepth = 20,
-        adapt_delta = 0.99
-    )
-
-    # save results
-    draws_df <- as_draws_df(samples$draws())
-    draws_df$subject <- sub
-
-    if (exists("output_df")) {
-        output_df <- rbind(output_df, draws_df)
-    } else {
-        output_df <- draws_df
+        if (exists("output_df")) {
+            output_df <- rbind(output_df, draws_df)
+        } else {
+            output_df <- draws_df
+        }
     }
 }
 print("------------")
@@ -108,5 +123,5 @@ print("------------")
 # write results
 out_path <- paste("../data/", model, "_samples.csv", sep = "")
 write.csv(output_df, out_path)
-print("output_df written to path:", out_path)
+print(paste("output_df written to path:", out_path))
 print("DONE")
