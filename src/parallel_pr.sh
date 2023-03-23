01590d8e41557582e9742226008a2becd492df08
saraoe <sara.m.oestergaard@gmail.com>
2023-03-23 16:01:44 +0000

    .log

--- src/parallel_pr.sh
+++ src/parallel_pr.sh
@@ -1,11 +1,15 @@
 # gcm
-Rscript parallel_pr.R gcm 1 104 binary &> ../logs/param_recov_gcm_1_104_binary &
-Rscript parallel_pr.R gcm 1 208 binary &> ../logs/param_recov_gcm_1_208_binary &
-Rscript parallel_pr.R gcm 1 312 binary &> ../logs/param_recov_gcm_1_312_binary &
-Rscript parallel_pr.R gcm 1 104 continuous &> ../logs/param_recov_gcm_1_104_continuous &
-Rscript parallel_pr.R gcm 1 208 continuous &> ../logs/param_recov_gcm_1_208_continuous &
+Rscript parallel_pr.R gcm 1 104 binary &> ../logs/param_recov_gcm_1_104_binary.log &
+Rscript parallel_pr.R gcm 1 208 binary &> ../logs/param_recov_gcm_1_208_binary.log &
+Rscript parallel_pr.R gcm 1 312 binary &> ../logs/param_recov_gcm_1_312_binary.log &
+Rscript parallel_pr.R gcm 1 104 continuous &> ../logs/param_recov_gcm_1_104_continuous.log &
+Rscript parallel_pr.R gcm 1 208 continuous &> ../logs/param_recov_gcm_1_208_continuous.log &
 
 # rl
-Rscript parallel_pr.R rl 1 104 binary &> ../logs/param_recov_rl_1_104_binary &
-Rscript parallel_pr.R rl 1 208 binary &> ../logs/param_recov_rl_1_208_binary &
-Rscript parallel_pr.R rl 1 312 binary &> ../logs/param_recov_rl_1_312_binary &
+Rscript parallel_pr.R rl 1 104 binary &> ../logs/param_recov_rl_1_104_binary.log &
+Rscript parallel_pr.R rl 1 208 binary &> ../logs/param_recov_rl_1_208_binary.log &
+Rscript parallel_pr.R rl 1 312 binary &> ../logs/param_recov_rl_1_312_binary.log &
+
+Rscript parallel_pr.R rl_simple 1 104 binary &> ../logs/param_recov_rl_simple_1_104_binary.log &
+Rscript parallel_pr.R rl_simple 1 208 binary &> ../logs/param_recov_rl_simple_1_208_binary.log &
+Rscript parallel_pr.R rl_simple 1 312 binary &> ../logs/param_recov_rl_simple_1_312_binary.log &
