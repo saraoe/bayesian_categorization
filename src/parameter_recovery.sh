d94afec925af0db44a3ed2047845a1280f507555
saraoe <sara.m.oestergaard@gmail.com>
2023-04-20 07:31:39 +0000

    rm type from sh

--- src/parameter_recovery.sh
+++ src/parameter_recovery.sh
@@ -1,14 +1,14 @@
 # gcm
-Rscript src/parameter_recovery.r gcm 1 104 binary &> logs/param_recov_gcm_1_104_binary.log &
-Rscript src/parameter_recovery.r gcm 1 208 binary &> logs/param_recov_gcm_1_208_binary.log &
-Rscript src/parameter_recovery.r gcm 1 312 binary &> logs/param_recov_gcm_1_312_binary.log &
+Rscript src/parameter_recovery.r gcm 1 104 &> logs/param_recov_gcm_1_104.log &
+Rscript src/parameter_recovery.r gcm 1 208 &> logs/param_recov_gcm_1_208.log &
+Rscript src/parameter_recovery.r gcm 1 312 &> logs/param_recov_gcm_1_312.log &
 
 # rl
-Rscript src/parameter_recovery.r rl 1 104 binary &> logs/param_recov_rl_1_104_binary.log &
-Rscript src/parameter_recovery.r rl 1 208 binary &> logs/param_recov_rl_1_208_binary.log &
-Rscript src/parameter_recovery.r rl 1 312 binary &> logs/param_recov_rl_1_312_binary.log &
+Rscript src/parameter_recovery.r rl 1 104 &> logs/param_recov_rl_1_104.log &
+Rscript src/parameter_recovery.r rl 1 208 &> logs/param_recov_rl_1_208.log &
+Rscript src/parameter_recovery.r rl 1 312 &> logs/param_recov_rl_1_312.log &
 
 # rl simple
-Rscript src/parameter_recovery.r rl_simple 1 104 binary &> logs/param_recov_rl_simple_1_104_binary.log &
-Rscript src/parameter_recovery.r rl_simple 1 208 binary &> logs/param_recov_rl_simple_1_208_binary.log &
-Rscript src/parameter_recovery.r rl_simple 1 312 binary &> logs/param_recov_rl_simple_1_312_binary.log &
+Rscript src/parameter_recovery.r rl_simple 1 104 &> logs/param_recov_rl_simple_1_104.log &
+Rscript src/parameter_recovery.r rl_simple 1 208 &> logs/param_recov_rl_simple_1_208.log &
+Rscript src/parameter_recovery.r rl_simple 1 312 &> logs/param_recov_rl_simple_1_312.log &
