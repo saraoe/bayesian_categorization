2108eefda7bc80752b31f083c318114be998d4dd
saraoe <sara.m.oestergaard@gmail.com>
2023-04-20 09:02:17 +0000

    gather rm index

--- src/gather_model_recovery.r
+++ src/gather_model_recovery.r
@@ -6,35 +6,32 @@ pacman::p_load(
 )
 
 # load data
-indexes <- c(1, 2, 3)
 true_models <- c("gcm", "rl", "rl_simple")
 
-for (i in indexes) {
-  for (true_model in true_models) {
-    tmp_compare <- read_csv(paste(
-      "data/recovery/model_recovery_loo_compare_",
-      true_model, "_", i, ".csv", sep = ""
-    ))
-    
-    if (exists("compare_df")) {
-      compare_df <- rbind(compare_df, tmp_compare)
-    } else {
-      compare_df <- tmp_compare
-    }
-    
-    tmp_pointwise <- read_csv(paste(
-      "data/recovery/model_recovery_loo_pointwise_",
-      true_model, "_", i, ".csv", sep = ""
-    )) %>% 
-      mutate(
-        trial = rep(seq_len(104), 3)
-      )
-    
-    if (exists("pointwise_df")) {
-      pointwise_df <- rbind(pointwise_df, tmp_pointwise)
-    } else {
-      pointwise_df <- tmp_pointwise
-    }
+for (true_model in true_models) {
+  tmp_compare <- read_csv(paste(
+    "data/recovery/model_recovery_loo_compare_",
+    true_model, "_", i, ".csv", sep = ""
+  ))
+  
+  if (exists("compare_df")) {
+    compare_df <- rbind(compare_df, tmp_compare)
+  } else {
+    compare_df <- tmp_compare
+  }
+  
+  tmp_pointwise <- read_csv(paste(
+    "data/recovery/model_recovery_loo_pointwise_",
+    true_model, "_", i, ".csv", sep = ""
+  )) %>% 
+    mutate(
+      trial = rep(seq_len(104), 3)
+    )
+  
+  if (exists("pointwise_df")) {
+    pointwise_df <- rbind(pointwise_df, tmp_pointwise)
+  } else {
+    pointwise_df <- tmp_pointwise
   }
 }
 
