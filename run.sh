5f31c966f384352aa55a703e792ff266d239e48a
SaraMøllerØstergaard#1413 <sara.m.oestergaard@gmail.com>
2023-04-23 14:46:08 +0200

    add validation to pipeline

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 # bash script for running all steps
+bash src/model_validation.sh
 bash src/parameter_recovery.sh
 bash src/model_recovery.sh
 bash src/fit_models.sh
---
+++ src/model_validation.sh
@@ -0,0 +1,3 @@
+Rscript src/model_validation.r gcm &> logs/model_validation_gcm.log &
+Rscript src/model_validation.r rl &> logs/model_validation_rl.log &
+Rscript src/model_validation.r rl_simple &> logs/model_validation_rl_simple.log &
